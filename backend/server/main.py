2dc276e2c960bdeaa1a191a6d1d0cf9c518da6d0
Pierre-Alexandre <pierre-alexandre.delannoy@sap.com>
2024-03-21 23:40:42 +0100

    add send and receive message from backend

--- backend/server/main.py
+++ backend/server/main.py
@@ -1,7 +1,7 @@
 import logging
 import requests
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from bs4 import BeautifulSoup
 from logger import logger
@@ -10,9 +10,16 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def test():
+def title():
     message = "MASP"
-    return 'MASP'
+    return message
+
+@app.route('/send-message', methods=['POST'])
+def send_message():
+    message = request.json.get('message')
+    print('Received message:', message)
+    modified_message = message + ' (this is a response from the backend!)'
+    return jsonify({'status': 'Message received!', 'modified_message': modified_message})
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
--- frontend/masp/src/index.ts
+++ frontend/masp/src/index.ts
@@ -1,13 +1,42 @@
 // @ts-ignore
-const MessageElement = document.getElementById('message') as HTMLElement;
+const messageElement = document.getElementById('title') as HTMLElement;
 
 fetch('http://127.0.0.1:5000')
     .then(response => response.text())
     .then(data => {
-        if (MessageElement) {
-            MessageElement.textContent = data;
+        if (messageElement) {
+            messageElement.textContent = data;
         }
     })
     .catch(error => {
         console.error('Error fetching data: ', error)
     })
+
+document.getElementById('messageForm')!.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const messageInput = (document.getElementById('messageInput') as HTMLInputElement).value;
+
+    try {
+        const response = await fetch('http://127.0.0.1:5000/send-message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: messageInput })
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            const modifiedMessage = responseData.modified_message;
+            (document.getElementById('messageStatus') as HTMLDivElement).textContent = `Message received: ${modifiedMessage}`;
+        }
+        else {
+            throw new Error('Failed to send message')
+        }
+    }
+
+    catch (error) {
+        console.error('Error sending message: ', error);
+        (document.getElementById('messageStatus') as HTMLDivElement).textContent = 'Failed to send message';
+    }
+})
