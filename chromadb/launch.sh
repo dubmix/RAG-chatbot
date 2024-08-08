b6ab82e5f03d8b3d7659c20b7e3c35234b1f0add
Pierre-Alexandre <pierre-alexandre.delannoy@sap.com>
2024-08-08 15:56:52 +0200

    dockerize chroma

--- chromadb/launch.sh
+++ chromadb/launch.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-chroma run --log-path ../logs/chroma.log
+set -e
+if docker images -q chroma > /dev/null 2>&1; then
+  echo "Image 'chroma' already exists, skipping build."
+else
+  echo "Image 'chroma' not found, building image."
+  docker build -t chroma .
+fi
+docker container rm -f chroma >> /dev/null 2>&1
+docker build -t chroma .
+docker run -d --name chroma -p "8000:8000" chroma
