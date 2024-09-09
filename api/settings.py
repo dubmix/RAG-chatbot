f315ea09fa6b9889fdaf851df61711232f726b90
Pierre-Alexandre <pierre-alexandre.delannoy@sap.com>
2024-09-09 13:29:26 +0200

    remove duplicate vars

--- api/settings.py
+++ api/settings.py
@@ -3,9 +3,6 @@ from typing import Literal
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-GPT_MODEL = "gpt-3.5-turbo"
-GPT_API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
-
 
 class Settings(BaseSettings):
     ENVIRONMENT: Literal["production", "local"] = "production"
