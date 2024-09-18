6736e22ee5aab3bb9ed6f923ee1edbc001c7a32c
Pierre-Alexandre <pierre-alexandre.delannoy@sap.com>
2024-09-18 13:46:15 +0200

    change overall font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,9 +8,11 @@ import Login from './Login.tsx';
 import Home from './Home.tsx';
 import './styles/app.css';
 
-const TIMEOUT = 30 * 1000;
+const TIMEOUT = 600000 * 1000;
 const VALIDATION_INTERVAL = 5 * 1000;
+// @ts-ignore
 const apiBaseHost = process.env.REACT_APP_BASE_URL || 'https://hilfy.co';
+// @ts-ignore
 const apiBasePort = process.env.REACT_APP_BASE_PORT || '';
 export const baseUrl = `${apiBaseHost}${apiBasePort}`;
 
