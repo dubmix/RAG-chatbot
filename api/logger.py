e084841a8fb0ec2c8fd05866648d77c1fe119049
Pierre-Alexandre <pierre-alexandre.delannoy@sap.com>
2024-12-21 17:29:45 +0100

    troubleshoot CircleCI pipeline

--- api/logger.py
+++ api/logger.py
@@ -1,12 +1,16 @@
 import datetime
 import logging
 
+from settings import Settings
+
 GREEN = "\033[92m"
 YELLOW = "\033[93m"
 RESET_PRINT = "\033[0m"
 
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H")
 filename = f"backend-{timestamp}.log"
+
+settings = Settings()
 
 
 class ColoredLogs(logging.StreamHandler):
@@ -19,12 +23,15 @@ class ColoredLogs(logging.StreamHandler):
         print(msg)
 
 
-file_handler = logging.FileHandler(f"../logs/{filename}")
-file_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
-file_handler.setFormatter(formatter)
-
-logger = logging.getLogger(__name__)
-logger.addHandler(file_handler)
-logger.addHandler(ColoredLogs())
-logger.setLevel(logging.DEBUG)
+if settings.ENVIRONMENT == "development":
+    file_handler = logging.FileHandler(f"../logs/{filename}")
+    file_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    logger = logging.getLogger(__name__)
+    logger.addHandler(file_handler)
+    logger.addHandler(ColoredLogs())
+    logger.setLevel(logging.DEBUG)
+else:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
