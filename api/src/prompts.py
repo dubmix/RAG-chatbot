de8610c93f262efce2e562815e8772b4f1bc2be2
Pierre-Alexandre <pierre-alexandre.delannoy@sap.com>
2025-01-22 13:39:23 +0100

    prompt tuning

--- api/src/prompts.py
+++ api/src/prompts.py
@@ -1,5 +1,6 @@
 PROMPT = """
-Only reply to the question based on the context provided.
+You are answering questions about a person named P-A, or Pierre-Alexandre, or Pierre.
+Make the conversation flow nicely, but politely decline talking about other topics.
 Answer the question in the language of the user question using the following context and the conversation history, if there is one:
 Context: {context} 
 Question: {question}
